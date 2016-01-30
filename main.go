c24714ec4c808498eea79c4e0d6e184657bbb235
emax <maxadau@gmail.com>
2016-01-30 18:59:04 +0100

    functionnal server

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"html/template"
+	"regexp"
+	"errors"
 )
 
 type Page struct {
@@ -30,42 +32,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Coucou %s !", r.URL.Path[1:])
 }
 
-func renderHTML(w http.ResponseWriter, p *Page, tplt string) {
 
-	t, err := template.ParseFiles(tplt)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, p *Page, tplt string) {
+
+	err := templates.ExecuteTemplate(w, tplt, p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	fmt.Println(title)
+	// title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+	if err != nil{
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
 
-	renderHTML(w, p, "view.html")
+	renderTemplate(w, p, "view.html")
 	// t, _ := template.ParseFiles("view.html")
 	// t.Execute(w, p)
 	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body )
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+	if err != nil{
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil{
 		p = &Page{Title : title}
 	}
 
-	renderHTML(w, p, "edit.html")
+	renderTemplate(w, p, "edit.html")
 	// t, _ := template.ParseFiles("edit.html")
 	// t.Execute(w, p)
 /*	fmt.Fprintf (w,"<h1>Editing %s</h1>"+
@@ -76,15 +82,29 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil{
+		return
+	}
 	body := r.FormValue("body")
 	p := &Page{Title : title, Body : []byte(body)}
-	err := p.save()
+	err = p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m ==nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
 }
 
 func main() {
