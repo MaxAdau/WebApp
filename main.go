fa9c83afe020533fcf28d7a42398edfd0fb4c607
emax <maxadau@gmail.com>
2016-01-30 17:34:33 +0100

    Implementing Html templates and error handling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"html/template"
 )
 
 type Page struct {
@@ -29,6 +30,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Coucou %s !", r.URL.Path[1:])
 }
 
+func renderHTML(w http.ResponseWriter, p *Page, tplt string) {
+
+	t, err := template.ParseFiles(tplt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	fmt.Println(title)
@@ -37,9 +51,41 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body )
+
+	renderHTML(w, p, "view.html")
+	// t, _ := template.ParseFiles("view.html")
+	// t.Execute(w, p)
+	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body )
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil{
+		p = &Page{Title : title}
+	}
+
+	renderHTML(w, p, "edit.html")
+	// t, _ := template.ParseFiles("edit.html")
+	// t.Execute(w, p)
+/*	fmt.Fprintf (w,"<h1>Editing %s</h1>"+
+        "<form action=\"/save/%s\" method=\"POST\">"+
+        "<textarea name=\"body\">%s</textarea><br>"+
+        "<input type=\"submit\" value=\"Save\">"+
+        "</form>", p.Title, p.Title, p.Body)*/
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title : title, Body : []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
 
 func main() {
 	// p1 := &Page{Title : "Coucou", Body : []byte("Ceci est mon body")}
@@ -51,6 +97,8 @@ func main() {
 	// http.HandleFunc("/", handler)
 	
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 
 	http.ListenAndServe(":8080", nil)
 
