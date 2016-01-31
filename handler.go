1d23833391e82dbfe042443b87979de8f0bc35ff
emax <maxadau@gmail.com>
2016-01-31 20:29:44 +0100

    Working on handling correctly

--- handler.go
+++ handler.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt" // just for debug
 	"net/http"
+	// "regexp" // used to check the URL validity
+	// "errors" // used to add errors
 	)
 
 func SetHandlers() {
-	http.HandleFunc("/Person", PersonHandler)
-	http.HandleFunc("/", Index)
+	//TODO : get "/person*"
+	http.HandleFunc("/person", PersonHandler)
+	// http.HandleFunc("/", Index)
 }
 
+
 func PersonHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Coucou %s !", r.URL.Path[1:])
+
+	var _ = PersonIndex(r.URL.Path[:])
+
+	// fmt.Fprintf(w, "Result %s !", result)
 	// Appel a Person.sdfasdf(r.URL.Path[1:])
 }
 
--- person.go
+++ person.go
@@ -1,10 +1,32 @@
 package main
 
+import (
+	"fmt" // Pour debug
+)
+
 type Person struct {
 	name string
 	Phone string
 	age int
 	
+}
+
+func PersonIndex(str string) string{
+	
+	// TODO : pourquoi rien ne s' affiche ?
+	fmt.Println("Coucou %s !", str)
+
+
+	// TODO : switch entres les differents operations
+		
+
+
+
+
+	/*coll := GetCollection("test", "people")
+	result := Person{}
+	coll.Find(bson.M{"name" : "Ale"}).One(&result)
+*/	return "coucou"
 }
 
 type Persons []Person
