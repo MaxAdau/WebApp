dea78a0371b751d450bafd4c4b7a2987207e4e6e
emax <maxadau@gmail.com>
2016-01-31 13:40:05 +0100

    cleaning and explaining code

--- main.go
+++ main.go
@@ -7,18 +7,22 @@ import (
 	"html/template"
 	"regexp"
 	// "errors"
+	"github.com/MaxAdau/lib"
 )
 
+// Declare a Strcut Page with two fields
 type Page struct {
 	Title string
 	Body []byte
 }
 
+// Save a Page into a file
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+// Return an *Page from a title
 func loadPage (title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
@@ -28,13 +32,17 @@ func loadPage (title string) (*Page, error) {
 	return &Page{Title : title, Body : body}, err
 }
 
+// Print the URL PATH. Currently unused
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Coucou %s !", r.URL.Path[1:])
 }
 
 
+// Declare a viariabla named template. This Must fonction throw a panic error
+// if thoses filenames are not found
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
+// Render HTML from the page name
 func renderTemplate(w http.ResponseWriter, p *Page, tplt string) {
 
 	err := templates.ExecuteTemplate(w, tplt, p)
@@ -43,6 +51,7 @@ func renderTemplate(w http.ResponseWriter, p *Page, tplt string) {
 	}
 }
 
+// Load a page , link to the /view/ URL
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	// title := r.URL.Path[len("/view/"):]
 	/*title, err := getTitle(w, r)
@@ -61,6 +70,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body )
 }
 
+// Edit a page, linked to the /edit/ URL
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 /*	title, err := getTitle(w, r)
 	if err != nil{
@@ -81,6 +91,7 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
         "</form>", p.Title, p.Title, p.Body)*/
 }
 
+// Save a Page, linked to the /save/ URL
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title : title, Body : []byte(body)}
@@ -92,6 +103,11 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
+// Handle view, edit and save Handlers
+// take a *Handler function on parameter, check if the title page is valid
+// through valid_path variable
+// Call the Handler func with the title as parameter
+// https://golang.org/ref/spec#Function_literals for more details
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
@@ -103,6 +119,8 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+// Declare a viariable that check the regexp. MustCompile Panic if regexp
+// is not fullfiled
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 /*func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
@@ -123,10 +141,11 @@ func main() {
 
 	// http.HandleFunc("/", handler)
 	
-	http.HandleFunc("/view/", makeHandler(viewHandler))
+	/*http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))*/
 
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil)
+	lib.PrintHello()
 
 }
